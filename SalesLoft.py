4fd74247496186101f725db8cdabbac59d4a4d86
anonymous <anonymous>
2018-11-03 15:26:27 -0400

    Extracted Name, Address, Email & added temporary function to verify this works correctly

--- SalesLoft.py
+++ SalesLoft.py
@@ -3,8 +3,6 @@ import requests
 
 authKey = os.environ['KEY']
 
-
-
 #Make a get request to SalesLoft people data and return the json contents
 def getPeopleData():
     urlApiPeople = "https://api.salesloft.com/v2/people.json"
@@ -16,4 +14,17 @@ def getPeopleData():
     data = apiRequest.json()
     return data
 
+#Temporary function for printing out the data; will be replaced by a function 
+#in the SalesLoftGui Class
+def printDict(dictData, colNames):
+    for item in dictData:
+        for col in colNames:
+            print(item[col], end = ' ')
+        print()
+            
+
 jsonPeopleData = getPeopleData()
+peopleData = jsonPeopleData['data']
+printDict(peopleData, ['first_name', 'last_name', 'email_address', 'title'])
+
+          
--- SalesLoftGui.py
+++ SalesLoftGui.py
@@ -1,13 +1,9 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk
 
 class SalesLoftGUI:
     def __init__(self, master):
         self.master = master
         master.title("SalesLoft People Data")
-
-        self.labelTitle = Label(master, text="SalesLoft People Data")
-        self.labelTitle.pack()
-
 
 root = Tk()
 my_gui = SalesLoftGUI(root)
