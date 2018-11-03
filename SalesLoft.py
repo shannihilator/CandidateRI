d6b9906a8977ea33bd08ef97f4ab2cdd9f7296ea
anonymous <anonymous>
2018-11-03 17:37:12 +0000

    API Request code

--- SalesLoft.py
+++ SalesLoft.py
@@ -1,4 +1,17 @@
 import os
-print(os.environ['KEY'])
+authKey = os.environ['KEY']
 
 import requests
+
+
+urlPeople = "https://api.salesloft.com/v2/people.json"
+
+headers = { 
+    'Authorization' : 'Bearer ' + authKey 
+}
+  
+r = requests.get(urlPeople, headers = headers  ) 
+print(r)
+
+data = r.json()
+print(data)
